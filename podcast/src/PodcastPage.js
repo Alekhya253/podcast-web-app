44149fc7dc02b66f2f4559915c3375cf1fd8a217
Alekhya253 <20wh1a05b6@bvrithyderabad.edu.in>
2023-04-23 22:39:35 +0530

    style

--- podcast/src/PodcastPage.js
+++ podcast/src/PodcastPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Client } from 'podcast-api';
+import "./PodcastPage.css"
 
 const MyComponent = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
@@ -12,7 +13,7 @@ const MyComponent = () => {
     const client = Client({ apiKey: '7947917a2172430eb271ffb603f58985' }); // Replace with your actual API key
     client.search({
       q: searchQuery,
-      page_size: 10,
+      page_size: 20,
     })
       .then((response) => {
         // Set the search results in the state
@@ -29,7 +30,8 @@ const MyComponent = () => {
   };
 
   return (
-    <div>
+<div>
+<div  style={{ backgroundColor:'#ffffff', textAlign: 'center' }} >
       <h1>Podcast Search</h1>
       <form onSubmit={handleSearchSubmit}>
         <input
@@ -37,16 +39,22 @@ const MyComponent = () => {
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.target.value)}
         />
+        <br></br>
+        <br></br>
         <button type="submit">Search</button>
       </form>
+      </div>
+      <div  style={{ backgroundColor:'rgb(27, 127, 103)', boxShadow: '0px 0px 5px rgba(0, 0, 255, 0.2)'}}  >
       {searchResults.length > 0 ? (
-        <ul>
+        <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
           {searchResults.map((podcast) => (
-            <li key={podcast.id}>
-              <h3>{podcast.title_original}</h3>
-              <p>Author: {podcast.publisher_original}</p>
-              <p>Date: {podcast.pub_date_ms}</p>
+            <li key={podcast.id}  style={{ flex: '0 0 25%', margin: 'auto', textAlign: 'center' }}>
+              {podcast.thumbnail && <img src={podcast.thumbnail} alt={podcast.title_original} />}
+              <div className='desc'>
+              <h3> <a href =  {podcast.listennotes_url} >{podcast.title_original}</a></h3>
+              <p>Author: {podcast.publisher_original ? podcast.publisher_original : 'Unknown'}</p>
               <button onClick={() => handlePlayPodcast(podcast)}>Play</button>
+              </div>
             </li>
           ))}
         </ul>
@@ -54,18 +62,18 @@ const MyComponent = () => {
         <p>No search results yet.</p>
       )}
       {selectedPodcast && (
-        <div>
+        <div style={{margin :'auto', textAlign:'center', color : '#ffffff'}}>
           <h2>Selected Podcast</h2>
           <h3>{selectedPodcast.title_original}</h3>
-          <p>Author: {selectedPodcast.publisher_original}</p>
-          <p>Date: {selectedPodcast.pub_date_ms}</p>
           <audio controls>
             <source src={selectedPodcast.audio} type="audio/mp3" />
             Your browser does not support the audio element.
           </audio>
         </div>
       )}
-    </div>
+      </div>
+      </div>
+    
   );
 };
 
